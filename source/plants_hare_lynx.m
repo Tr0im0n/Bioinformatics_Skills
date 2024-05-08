2a0bbd1167582b153b0be5b106dcb66c6a8f7098
SabScuo <152314495+SabScuo@users.noreply.github.com>
2024-05-08 10:48:57 +0200

    Sabrina
    i made some changes

--- source/plants_hare_lynx.m
+++ source/plants_hare_lynx.m
@@ -6,7 +6,7 @@ dydt = zeros(3,1);
 
 dydt(1) = y(1)*(1-y(1)) - ((a1*y(1))/(1+b1*y(1)))*y(2);
 %the per herbivore consumption of plants saturates (reaches max value) with 
-% increasing plant density.
+% increasing plant density.  &changesssss
 dydt(2) = ((a1*y(1))/(1+b1*y(1)))*y(2)-d1*y(2)-(a2*y(2)/(1+b2*y(2)))*y(3);
 dydt(3) = (a2*y(2)/(1+b2*y(2)))*y(3) - d2*y(3);
 %The rate at which the predator consumes the herbivore is a saturating 
